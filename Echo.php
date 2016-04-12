db8f8745aeb9f3288cfb8136f4cd3e245efd7ed1
sohan709 <sohan709@gmail.com>
2016-04-12 13:29:02 +0530

    Added files via upload

---
+++ Echo.php
@@ -0,0 +1,32 @@
+<?php
+$test="Nehan";
+print '<h1> hello</h1>';
+echo '<h5> hello</h5>';
+#how to use INPUT text embedded in php
+echo '<label> Name:</label>';echo ' <input type="text" name="text1" value="Sohan">';
+'<\br>'
+
+?>
+<?php#how to use PHP embedded in INPUT?>
+</br><input type="text" name="text1" value="<?php echo $test;?>"> </br>
+
+
+<?php
+echo "My Name is " .$test. " and am not a Terrorist \n"; 
+'</br';
+#Die will kill all the other codes below it
+ $users=$_SERVER['REMOTE_ADDR'];
+ #To show Ip Address
+ echo $users;
+die();
+?>
+
+
+<?php
+echo "My Name is " .$test. "and am rrsdaorizdst";
+
+?>
+
+
+
+
